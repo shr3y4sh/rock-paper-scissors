f6439483f737077caa9e84a21a0aebe122e35b48
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2024-12-25 10:12:14 +0530

    feat: playRound function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 console.log("Hello World!");
 
-let gameChoices = ["rock", "paper", "scissors"];
+const gameChoices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+let winHumanOrComputer;
 
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
@@ -15,11 +19,26 @@ function getHumanChoice() {
   return humanChoice.toLowerCase();
 }
 
+function playRound(human, computer) {
+  if (human === computer) {
+    console.log(`Draw! You both chose ${human}`);
+    winHumanOrComputer = "none";
+    return;
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (human === gameChoices[i]) {
+      if (computer === gameChoices[(i + 1) % 3]) {
+        console.log(`Loss! ${computer} beats ${human}`);
+        winHumanOrComputer = "computer";
+        return;
+      } else if (computer === gameChoices[(i + 2) % 3]) {
+        console.log(`Win! ${human} beats ${computer}`);
+        winHumanOrComputer = "human";
+        return;
+      }
+    }
+  }
+}
+
 // console.log(getHumanChoice());
-
-let humanScore = 0;
-let computerScore = 0;
-
-let winHumanOrComputer;
-
-let gameOutcomes = ["lose", "win", "draw"];
