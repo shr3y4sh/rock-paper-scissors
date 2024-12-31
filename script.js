34126c4c717ef42a6cb8f264de904ae564f1f8ee
shr3y4sh <67783996+shr3y4sh@users.noreply.github.com>
2024-12-31 14:35:17 +0530

    feat: user and comp objects, user.choice > string
    used objects and added name, choice, score
    updated function to input objects and check for user.choice whic is a string
    display scores

--- script.js
+++ script.js
@@ -4,22 +4,57 @@ PSUEDOCODE:
 1. user clicks on one button rock paper scissors
 2. computer makes one random choice on click
 3. check the choices and score user or computer
+4. make outcome choice, append it on html
+5. if win => user++; loss => computer++ draw nothing;
 */
 
-let user, computer;
+let user = {
+  name: "YOU",
+  score: 0,
+};
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
+let computer = {
+  name: "I",
+  score: 0,
+};
 
-let userScore, computerScore;
 let choice = document.querySelector("#option");
-const gameChoices = [rock, paper, scissors];
+let scoreList = document.querySelector("#score-list");
+let final = document.querySelector("#final");
+
+const gameChoices = ["Rock", "Paper", "Scissors"];
 
 choice.addEventListener("click", function (e) {
-  const option = e.target;
-  const computer = computerChoice();
+  user.choice = e.target.textContent;
+  computer.choice = computerChoice();
+
+  document.querySelector("#user-choice").textContent =
+    "YOU: " + user.choice.toUpperCase();
+  document.querySelector("#computer-choice").textContent =
+    "ME: " + computer.choice.toUpperCase();
+
+  playRound(user, computer);
+  displayScore(user, computer);
+
+  if (user.score === 5 || computer.score === 5) {
+    const winner = scoreEval(user, computer);
+    const verdict = document.createElement("h1");
+    verdict.textContent = "GAME OVER";
+    final.insertBefore(verdict, scoreList);
+    verdict = document.createElement("h2");
+    verdict.textContent = `${winner.name} WON`;
+    final.insertBefore(verdict, scoreList);
+    choice.remove();
+  }
 });
+
+function scoreEval(user, computer) {
+  if (user.score > computer.score) {
+    return user;
+  } else if (user.score < computer.score) {
+    return computer.textContent;
+  }
+}
 
 function computerChoice() {
   const comp = Math.floor(Math.random() * 3);
@@ -27,19 +62,28 @@ function computerChoice() {
 }
 
 function playRound(user, computer) {
-  if (user === computer) {
-    return "draw";
+  console.log("round played");
+  console.log(user.choice);
+  console.log(computer.choice);
+
+  if (user.choice === computer.choice) {
+    return;
   }
 
   for (let i = 0; i < 3; i++) {
-    if (user === gameChoices[i]) {
-      if (computer === gameChoices[(i + 1) % 3]) {
-        console.log(`Loss! ${computer} beats ${user}`);
-        return "computer";
-      } else if (computer === gameChoices[(i + 2) % 3]) {
-        console.log(`Win! ${user} beats ${computer}`);
-        return "user";
+    if (user.choice === gameChoices[i]) {
+      if (computer.choice === gameChoices[(i + 1) % 3]) {
+        computer.score++;
+      } else if (computer.choice === gameChoices[(i + 2) % 3]) {
+        user.score++;
       }
     }
   }
+}
+
+function displayScore(user, computer) {
+  const list = document.createElement("li");
+
+  list.textContent = `YOUR SCORE: ${user.score}  MY SCORE: ${computer.score}`;
+  scoreList.insertBefore(list, scoreList.childNodes[0]);
 }
